2ab964f69d41b934e54a978ff415cf6aabd73be0
yihanw <y2349wan@uwaterloo.ca>
2016-08-23 15:20:11 -0400

    Update A1Q1_Jane.java

--- src/A1Q1_Jane.java
+++ src/A1Q1_Jane.java
@@ -1,7 +1,7 @@
 /*Jane Wang
-  spt.27
+  Sept.27.2014
   A1Q1_Jane 
-  the program will calculate the area and the circumference of a circle*/
+  This program calculates the area and the circumference of a circle based on the value of radius entered by a user*/
 //The "Assignment1" class.
 import java.awt.*;
 import hsa.Console;
@@ -9,7 +9,7 @@ import hsa.Console;
 public class A1Q1_Jane
 {
     static Console c;           // The output console
-
+    // Place your program here.  'c' is the output console
     public static void main (String[] args)
     {
 	c = new Console ();
@@ -25,7 +25,5 @@ public class A1Q1_Jane
 	C = 2*PI*R;
 	c.print("the circumference is ");
 	c.print(C);
-
-	// Place your program here.  'c' is the output console
     } // main method
 } // Assignment class
