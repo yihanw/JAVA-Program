c67b35c4c6b76dc116b94ce8d60b8562f5869d93
yihanw <y2349wan@uwaterloo.ca>
2016-08-23 15:26:30 -0400

    Update Aloop3_Jane.java

--- src/Aloop3_Jane.java
+++ src/Aloop3_Jane.java
@@ -1,8 +1,7 @@
 /*Jane Wang
   Dec.3.2013
   Aloop3_Jane
-  this program will ask user for number of the month and the start date,
-  then provide a calender*/
+  This program asks user to enter the number of the month and the start date, and provides a calender*/
 //The "Aloop3_Jane" class
 import java.awt.*;
 import hsa.Console;
@@ -30,37 +29,37 @@ public class Aloop3_Jane
 
 	if (date.equals ("sunday"))
 	{
-	    space = 0;//number of space in the first line
+	    space = 0; //number of space in the first line
 	}
 	if (date.equals ("monday"))
 	{
 	    c.print ("\t");
-	    space = 1;//number of space in the first line
+	    space = 1; //number of space in the first line
 	}
 	if (date.equals ("tuesday"))
 	{
 	    c.print ("\t\t");
-	    space = 2;//number of space in the first line
+	    space = 2; //number of space in the first line
 	}
 	if (date.equals ("wednesday"))
 	{
 	    c.print ("\t\t\t");
-	    space = 3;//number of space in the first line
+	    space = 3; //number of space in the first line
 	}
 	if (date.equals ("thursday"))
 	{
 	    c.print ("\t\t\t\t");
-	    space = 4;//number of space in the first line
+	    space = 4; //number of space in the first line
 	}
 	if (date.equals ("friday"))
 	{
 	    c.print ("\t\t\t\t\t");
-	    space = 5;//number of space in the first line
+	    space = 5; //number of space in the first line
 	}
 	if (date.equals ("saturday"))
 	{
 	    c.print ("\t\t\t\t\t\t");
-	    space = 6;//number of space in the first line
+	    space = 6; //number of space in the first line
 	}
 
 	for (i = 1 ; i <= num ; i++)
@@ -73,13 +72,5 @@ public class Aloop3_Jane
 		space = 0;
 	    }
 	}
-
-
-
-
-
-
-
-	// Place your program here.  'c' is the output console
     } // main method
 } // Aloop3_Jane class
