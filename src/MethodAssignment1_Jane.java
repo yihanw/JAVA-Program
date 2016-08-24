f75d6e73f27cfa5d56cd38f7feeccaa7cc91035a
yihanw <y2349wan@uwaterloo.ca>
2016-08-24 11:42:07 -0400

    Update MethodAssignment1_Jane.java

--- src/MethodAssignment1_Jane.java
+++ src/MethodAssignment1_Jane.java
@@ -1,8 +1,7 @@
 /*Jane Wang
- *Jan.17.14
- *MethodAssignment1_Jane
- *This program will continue asking user to enter a string and a positon,
-  then display the character.
+  Jan 17, 2014
+  MethodAssignment1_Jane
+  This program continues asking user to enter a string and a positon, then displays the character.
 */  
 // The "MethodAssignment1_Jane" class.
 import java.awt.*;
@@ -39,8 +38,6 @@ public class MethodAssignment1_Jane
 	    n = c.readInt ();
 	} while (n >= 0);
 	c.println("end of the program");//if position is negetive
-    
-	// Place your program here.  'c' is the output console
     } // main method
 
     public static char getLetter (String txt, int n)
